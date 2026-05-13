57432fa8b71e90ab8bc4e0c2f99fa24c2ed43915
Dmwsec <brownboi@protonmail.com>
2026-05-13 18:05:04 +0530

    Add tool registry

---
+++ core/tools.py
@@ -0,0 +1,20 @@
+TOOLS = {
+    'safe': [
+        'subfinder', 'httpx', 'katana', 'gau', 'waybackurls', 'subjs'
+    ],
+    'approval': [
+        'ffuf', 'arjun', 'dalfox', 'nmap', 'nuclei'
+    ],
+    'manual': [
+        'sqlmap', 'commix', 'race-testing', 'request-smuggling'
+    ]
+}
+
+
+def list_tools():
+    print('\n[Legion Tool Registry]\n')
+    for level, tools in TOOLS.items():
+        print(f'{level.upper()}:')
+        for tool in tools:
+            print(f'  - {tool}')
+        print()
